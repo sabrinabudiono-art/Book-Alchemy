8cc5e42100f435a96b7aa50de9185e7d7163822b
sabrinabudiono-art <sabrinabudiono@gmail.com>
2026-03-01 02:16:52 +0100

    added homepage

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from data_models import db, Author, Book, session
+from data_models import db, Author, Book
 
 app = Flask(__name__)
 
@@ -10,10 +10,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
 db.init_app(app)
 
-
 @app.route('/')
 def home():
-    return render_template('home.html')
+    books_author_tuple = db.session.query(Book, Author) \
+        .join(Author, Book.author_id == Author.author_id) \
+        .all()
+    return render_template('home.html', books_author_tuple=books_author_tuple)
 
 
 @app.route('/add_author', methods=['GET', 'POST'])
@@ -24,24 +26,26 @@ def add_author():
             author_date_of_birth=request.form.get('birthdate'),
             author_date_of_death=request.form.get('date_of_death'))
 
-        session.add(new_author)
-        session.commit()
-        return redirect('home.html')
+        db.session.add(new_author)
+        db.session.commit()
+        return redirect(url_for('home'))
     return render_template('add_author.html')
 
 
-@app.route('/add_book')
+@app.route('/add_book', methods=['GET', 'POST'])
 def add_book():
     if request.method == 'POST':
         new_book = Book(
             book_isbn=request.form.get('isbn'),
             book_title=request.form.get('title'),
-            book_publication_year=request.form.get('publication_year'))
+            book_publication_year=request.form.get('publication_year'),
+            author_id = int(request.form.get('author_id')))
 
-        session.add(new_book)
-        session.commit()
-        return redirect('home.html')
-    return render_template('add_book.html')
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    authors = Author.query.all()
+    return render_template('add_book.html', authors=authors)
 
 
 #with app.app_context():
